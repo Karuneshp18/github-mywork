5fa40c961837371037485a5679ad2c9ee9730187
Karunesh <karuneshp94@gmail.com>
2025-05-12 21:43:54 +0530

    Initial commit

---
+++ 1st_CODE.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// main() function: where the execution of
+// C++ program begins
+int main() {
+  
+    // This statement prints "Hello World"
+    cout << "Hello World";
+    cout << "Karunesh";
+
+    return 0;
+}
